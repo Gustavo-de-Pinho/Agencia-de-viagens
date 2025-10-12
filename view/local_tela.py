e778a534b2a34427e284e4c0df87d54bd3ba3513
Gustavo de Pinho <tavinhopinho@gmail.com>
2025-10-12 16:15:03 -0300

    Create local_tela.py
    criando uma tela do local para que funcione

---
+++ view/local_tela.py
@@ -0,0 +1,29 @@
+class LocalTela:
+    def mostra_opcoes(self) -> int:
+        print("\n=========== LOCAIS ===========")
+        print("> (1) Cadastrar Local (associar a uma cidade)")
+        print("> (2) Listar Locais")
+        print("> (3) Adicionar Passeio a um Local")
+        print("> (4) Excluir Local")
+        print("----------------------------")
+        print("> (0) Retornar")
+        
+        return int(input("Escolha uma opção: "))
+    
+    def mostra_mensagem(self, msg: str):
+        print(msg)
+
+    def seleciona_cidade(self) -> str:
+        return input("Informe o nome da cidade do local: ")
+
+    def seleciona_passeio(self) -> str:
+        return input("Informe o nome do passeio que deseja adicionar: ")
+
+    def mostra_local(self, dados: dict):
+        print(f"Local: {dados['cidade']}")
+        if dados['passeios']:
+            print("   Passeios disponíveis:")
+            for passeio in dados['passeios']:
+                print(f"     - {passeio}")
+        else:
+            print("Nenhum passeio associado a este local")
