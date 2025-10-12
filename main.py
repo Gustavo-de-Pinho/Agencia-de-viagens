7fdb6353acb31a8e1d5d988efc04f12528077fdd
Gustavo de Pinho <tavinhopinho@gmail.com>
2025-10-12 16:19:51 -0300

    Update main.py
    Alteração na Main para testarmos o controlador de local

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ from control.sistema_controlador import SistemaControlador
 
 sistema = SistemaControlador()
 
-#sistema.pessoa_controlador.abre_tela()
-#sistema.grupo_controlador.abre_tela()
-#sistema.passagem_controlador.run()
-
-sistema.pagamento_controlador.abre_tela()
+sistema.local_controlador.abre_tela()
