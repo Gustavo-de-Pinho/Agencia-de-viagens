0a251d5242fb68293af5baf292a8a2e757f7d85d
Gustavo de Pinho <tavinhopinho@gmail.com>
2025-10-11 21:51:43 -0300

    Update sistema_controlador.py
    Atualizando sistema para que possamos testar o transporte (tela e controlador)

--- control/sistema_controlador.py
+++ control/sistema_controlador.py
@@ -2,7 +2,8 @@ from control.grupo_controlador import GrupoControlador
 #from control.pacote_controlador import PacoteControlador
 #rom control.pagamento_controlador import PagamentoControlador
 from control.pessoa_controlador import PessoaControlador
-from control.passagem_controlador import PassagemController
+#from control.passagem_controlador import PassagemController
+from control.transporte_controlador import TransporteControlador
 
 class SistemaControlador:
     def __init__(self):
@@ -10,7 +11,8 @@ class SistemaControlador:
         #self.__pacote_controlador = PacoteControlador(self)
         #self.__pagamento_controlador = PagamentoControlador(self)
         self.__pessoa_controlador = PessoaControlador(self)
-        self.__passagem_controlador = PassagemController(self)
+        #self.__passagem_controlador = PassagemController(self)
+        self.__transporte_controlador = TransporteControlador(self)
 
     @property
     def grupo_controlador(self):
@@ -28,7 +30,11 @@ class SistemaControlador:
     def pessoa_controlador(self):
         return self.__pessoa_controlador
     
-    @property
-    def passagem_controlador(self):
+    #@property
+    #def passagem_controlador(self):
         return self.__passagem_controlador
+    
+    @property
+    def transporte_controlador(self):
+        return self.__transporte_controlador
     
